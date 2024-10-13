a6904a5280a569fe4f5bc4838831b2c821608ef4
efoppiano <efoppiano@fi.uba.ar>
2024-10-13 19:49:06 -0300

    feat: add P2, P3 and P4 rooms to search bar

--- fiuba-guide/src/assets/floorConfig.ts
+++ fiuba-guide/src/assets/floorConfig.ts
@@ -1,5 +1,8 @@
 import { FloorId } from "../types/FloorId";
 import P1config from "./P1.json";
+import P2config from "./P2.json";
+import P3config from "./P3.json";
+import P4config from "./P4.json";
 
 export type FloorConfig = {
   id: string;
@@ -9,4 +12,7 @@ export type FloorConfig = {
 
 export const floorConfig = {
   [FloorId.P1]: P1config,
+  [FloorId.P2]: P2config,
+  [FloorId.P3]: P3config,
+  [FloorId.P4]: P4config,
 };
