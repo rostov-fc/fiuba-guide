70d3c35ec7b27dd9439346b76dbb7416f4f909b2
efoppiano <efoppiano@fi.uba.ar>
2024-10-14 20:31:49 -0300

    fix: hide background rectangle from SVGs

--- fiuba-guide/src/components/Floor.tsx
+++ fiuba-guide/src/components/Floor.tsx
@@ -7,16 +7,25 @@ type Props = {
 };
 
 const colorSelectedRoom = (svg: SVGSVGElement, selectedRoom: RoomSearchData | null) => {
-  if (!selectedRoom) return;
-
   // we search all nodes for one with inkscape:label equal to selectedRoom
   // we can not use id, because it is changed by the library when injected
   // we can not use querySelector because it breaks the image
   const nodes = [...svg.getElementsByTagName("*")] as HTMLElement[];
 
   for (const node of nodes) {
-    if (node.getAttribute("inkscape:label") === selectedRoom.room) {
-      node.style.fill = "red";
+    const label = node.getAttribute("inkscape:label");
+    if (label === selectedRoom?.room) {
+      node.style.fill = "#314c96";
+    } else if (node.style?.fill) {
+      if (label?.startsWith("BACKGROUND")) {
+        node.style.fill = "#0000";
+        node.style.stroke = "#0000";
+      }
+      else if (label?.startsWith("WALL")) {
+        node.style.fill = "#0000";
+      } else {
+        node.style.fill = "#c2c2c2";
+      }
     }
   }
 };
