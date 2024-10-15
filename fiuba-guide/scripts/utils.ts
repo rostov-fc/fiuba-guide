d20d4743d2a905b682ef1e217b74097b905b5f8b
efoppiano <efoppiano@fi.uba.ar>
2024-10-15 15:27:50 -0300

    feat: add scripts to remove styles from SVGs and add custom label

---
+++ fiuba-guide/scripts/utils.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+export enum FloorId {
+    P1 = "P1",
+    P2 = "P2",
+    P3 = "P3",
+    P4 = "P4",
+}
+
+export async function backUpFiles(originPath: string, destPath: string) {
+    try {
+        await fs.promises.stat(destPath);
+    } catch (e) {
+        await fs.promises.mkdir(destPath, { recursive: true });
+    }
+    (await fs.promises.readdir(originPath)).map(async (path) => {
+        if (!(await fs.promises.stat(`${originPath}/${path}`)).isDirectory()) {
+            await fs.promises.copyFile(`${originPath}/${path}`, `${destPath}/${path}.bak`);
+        }
+    });
+}
+
+export const originPath = process.env.ORIGIN_PATH || "public/floorplans/svg";
+export const destPath = process.env.DEST_PATH || "public/floorplans/svg";
+
+export const backUpDir = process.env.BACKUP_DIR || "public/floorplans/svg/backup";
