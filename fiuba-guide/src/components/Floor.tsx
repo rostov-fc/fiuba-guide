1f5425bae392f3919f1f68bbd228482c86f6ebac
Elián Foppiano <65984167+efoppiano@users.noreply.github.com>
2024-10-19 15:07:37 -0300

    feat: show room names
    * feat: show room names at the center of the room bbox with text wrapping
    
    * fix: show room displayText instead of id
    
    * refactor: binary search to find room by id

--- fiuba-guide/src/components/Floor.tsx
+++ fiuba-guide/src/components/Floor.tsx
@@ -1,7 +1,9 @@
 import { ReactSVG } from "react-svg";
 import { FloorId } from "../types/FloorId";
 import { RoomSearchData } from "./SearchBar";
+import { insertWrappedTextInSvg } from "../utils";
 import "./Floor.css"
+import { searchById } from "../assets/floorConfig";
 
 type Props = {
   selectedRoom: RoomSearchData | null;
@@ -11,13 +13,30 @@ const colorSelectedRoom = (svg: SVGSVGElement, selectedRoom: RoomSearchData | nu
   // we search all nodes for one with label equal to selectedRoom
   // we can not use id, because it is changed by the library when injected
 
+  const selectedFloor = selectedRoom ? selectedRoom.floorId : FloorId.P1;
+
   (svg.querySelectorAll("rect,path") as NodeListOf<SVGElement>)
     .forEach((node) => {
       const label = node.getAttribute("label");
+      if (!label) {
+        return;
+      }
+
       if (label === selectedRoom?.room) {
         node.style.removeProperty("fill");
         node.classList.add("selected");
       }
+
+      if (label?.startsWith("BACKGROUND") ||
+        label?.startsWith("WALL") ||
+        label?.startsWith("UNK")) {
+        return;
+      }
+
+      const displayName = searchById(selectedFloor, label)?.displayName;
+
+      insertWrappedTextInSvg(node as SVGGraphicsElement,
+        displayName || "", label === selectedRoom?.room ? ["selected-text"] : []);
     });
 };
 
