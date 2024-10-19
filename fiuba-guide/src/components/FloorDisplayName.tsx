aaaf071ed08c4f3282d58ccea6da46aad2998fc7
efoppiano <efoppiano@fi.uba.ar>
2024-10-19 15:30:52 -0300

    refactor: FloorDisplayName

--- fiuba-guide/src/components/FloorDisplayName.tsx
+++ fiuba-guide/src/components/FloorDisplayName.tsx
@@ -4,21 +4,13 @@ type Props = {
     floorId: FloorId;
 }
 
-export const FloorDisplayName = ({ floorId }: Props) => {
-    return (
-        getDisplayName(floorId)
-    )
+const floorDisplayNameMap = {
+    [FloorId.P1]: <div>1<sup>er</sup> Piso</div>,
+    [FloorId.P2]: <div>2<sup>do</sup> Piso</div>,
+    [FloorId.P3]: <div>3<sup>er</sup> Piso</div>,
+    [FloorId.P4]: <div>4<sup>to</sup> Piso</div>
 }
 
-function getDisplayName(floorId: FloorId): JSX.Element {
-    switch (floorId) {
-        case FloorId.P1:
-            return <div>1<sup>er</sup> Piso</div>
-        case FloorId.P2:
-            return <div>2<sup>do</sup> Piso</div>
-        case FloorId.P3:
-            return <div>3<sup>er</sup> Piso</div>
-        case FloorId.P4:
-            return <div>4<sup>to</sup> Piso</div>
-    }
+export const FloorDisplayName = ({ floorId }: Props) => {
+    return floorDisplayNameMap[floorId];
 }
